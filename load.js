6b760c73cb69ee7ebba21d62c5b546c91e8a3f1f
dvdrtrgn <dvdrtrgn@gmail.com>
2014-08-19 13:43:03 -0500

    comment out wf fonts

--- load.js
+++ load.js
@@ -68,12 +68,12 @@ Glob = new Global('Glob');
     Load.font = {
         test: ROOT.conf.nom === 'localhost' || ROOT.conf.nom === 'qla1',
         yep: [
-            // G.lib + 'fonts/archer.ssm.css',
-            // G.lib + 'fonts/archer.ssm.itl.css',
+            /* G.lib + 'fonts/archer.ssm.css', */
+            /* G.lib + 'fonts/archer.ssm.itl.css', */
         ],
         nope: [
             /* '//cloud.typography.com/6819872/620964/css/fonts.css', Normal */
-            // '//cloud.typography.com/6819872/633184/css/fonts.css', /* ScrnSmrt */
+            /* '//cloud.typography.com/6819872/633184/css/fonts.css',  ScrnSmrt */
         ],
     };
 
