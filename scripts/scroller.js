fe4f5928d70b61de983f7e1f4630e8931d90c2ef
dvdrtrgn <dvdrtrgn@gmail.com>
2014-08-19 11:26:58 -0500

    decommission the click stopper

--- scripts/scroller.js
+++ scripts/scroller.js
@@ -56,27 +56,12 @@ var Scroller = (function ($, G, U) { // IIFE
         if (U.debug(2)) {
             C.debug(name, '_autoScroll', scroller);
         }
-        var interva, indicat;
-
         if (!scroller.pages) {
             return;
         }
-
-        interva = W.setInterval(function () {
+        return W.setInterval(function () {
             scrollNext(scroller);
         }, Df.speed);
-
-        indicat = W.isIE ? scroller.indicator1 : scroller.indicators[0];
-
-        $(indicat.wrapper) //
-        .parent() //
-        .one('click keypress touchend', function () {
-            if (U.debug(2)) {
-                C.debug(name, 'click keypress touchend', scroller);
-            }
-            $(this).find('.control').trigger('toggle');
-        });
-        return interva;
     }
 
     function _attachPort(sel) {
