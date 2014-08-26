be606864a7d0a87e5dfa4e042b950c0ec130cbca
dvdrtrgn <dvdrtrgn@gmail.com>
2014-08-26 00:24:31 -0500

    move stuff into place

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,33 @@
 module.exports = function(grunt) {
+
     // Utility to load the different option files based on their names
-
-
-    // Start initial config object
-    var config = {
-        pkg: grunt.file.readJSON('package.json')
-    };
-
-    // Load tasks from the tasks folder
-    grunt.loadTasks('tasks');
-
-    // Load tasks/options by the name: watch.js => watch{}
-    grunt.util._.extend(config, function (path) {
+    function synthobj(path) {
         var glob, object, key;
 
         glob = require('glob');
         object = {};
+
         glob.sync('*', {
             cwd: path
         }).forEach(function(option) {
             key = option.replace(/\.js$/,'');
             object[key] = require(path + option);
         });
-
         return object;
-    }('./tasks/options/'));
+    }
+
+    // Start initial config object
+    var config = {
+        pkg: grunt.file.readJSON('./package.json')
+    };
+
+    // Load tasks from the tasks folder
+    // Load tasks/options by the name: watch.js => watch{}
+    grunt.loadTasks('tasks');
+    grunt.util._.extend(config, synthobj('./tasks/options/'));
 
     grunt.initConfig(config);
+    // grunt.loadNpmTasks('grunt-contrib-compass');
+
     require('load-grunt-tasks')(grunt);
 };
