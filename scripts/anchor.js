a0e45ae7d71cef3efd8a5bcb6889e012762e44e0
dvdrtrgn <dvdrtrgn@gmail.com>
2014-10-02 13:35:10 -0500

    strip relic of index removal

--- scripts/anchor.js
+++ scripts/anchor.js
@@ -41,13 +41,6 @@ var Anchor = (function ($, G, U) { // IIFE
     }
 
     function _write(str) {
-        var tmp = L.pathname.split('/');
-
-        if (tmp.pop()) { // document fragment
-            C.warn(name, '_write', tmp);
-            //L.pathname = tmp.join('/') + '/'; // rewrite without document
-        }
-
         L.hash = '!' + str;
     }
 
